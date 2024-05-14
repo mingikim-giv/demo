4ee428f38b543c355d3ce8a59a8474eb0dca8410
mingikim-giv <mingikim.giv@gmail.com>
2024-05-14 18:13:54 +0900

    Modify

--- src/router.js
+++ src/router.js
@@ -17,78 +17,78 @@ GET     /demo/book/search           검색 도서 목록 페이지
 GET     /demo/book/search/{:isbn}   검색 도서 상세 페이지 
 */
 const router = createBrowserRouter(
-[
-{
-    path: "/",
-    element: <Root />,
-    children: [
-    {
-        path: "/",
-        element: <Home />,
-    },
+    [
+        {
+            path: "/",
+            element: <Root />,
+            children: [
+                {
+                    path: "/",
+                    element: <Home />,
+                },
+            ],
+            errorElement: <>
+                <h1>Opps!!!</h1>
+            </>
+        },
+        {
+            path: "/video",
+            element: <Root />,
+            children: [
+                {
+                    path: "/video",
+                    element: (
+                        <VideoList />
+                    ),
+                },
+                {
+                    path: "/video/list",
+                    element: (
+                        <VideoList />
+                    ),
+                },
+                {
+                    path: "/video/search",
+                    element: (
+                        <VideoList />
+                    ),
+                },
+            ],
+        },
+        {
+            path: "/book",
+            element: <Root />,
+            children: [
+                {
+                    path: "/book",
+                    element: (
+                        <BookList />
+                    ),
+                },
+                {
+                    path: "/book/list",
+                    element: (
+                        <BookList />
+                    ),
+                },
+                {
+                    path: "/book/search",
+                    element: (
+                        <BookList />
+                    ),
+                },
+                {
+                    path: "/book/search/:isbn",
+                    element: (
+                        <BookDetail />
+                    ),
+                },
+            ],
+        },
     ],
-    errorElement: <>
-    <h1>Opps!!!</h1>
-    </>
-},
-{
-    path: "/video",
-    element: <Root />,
-    children: [
     {
-        path: "/video",
-        element: (
-            <VideoList />
-        ),
-    },
-    {
-        path: "/video/list",
-        element: (
-            <VideoList />
-        ),
-    },
-    {
-        path: "/video/search",
-        element: (
-            <VideoList />
-        ),
-    },
-    ],
-},
-{
-    path: "/book",
-    element: <Root />,
-    children: [
-    {
-        path: "/book",
-        element: (
-            <BookList />
-        ),
-    },
-    {
-        path: "/book/list",
-        element: (
-            <BookList />
-        ),
-    },
-    {
-        path: "/book/search",
-        element: (
-            <BookList />
-        ),
-    },
-    {
-        path: "/book/search/:isbn",
-        element: (
-            <BookDetail />
-        ),
-    },
-    ],
-},
-],
-{
-basename: "/demo",
-}
+        basename: "/demo",
+    }
 );
 
 export default router;
